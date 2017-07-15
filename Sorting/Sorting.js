c8d4569105be88b59e475f5f7e46ac575c4b8c04
Vũ Đức Duy <duyisking1060@gmail.com>
2017-07-15 11:21:22 +0700

    Add heapsort algorithm
    Heapsort is now available.
    mainJS.js is used to test the algorithm and compare the performance between algorithms.

--- Sorting/Sorting.js
+++ Sorting/Sorting.js
@@ -48,3 +48,91 @@ function bubbleSort(list, operator, reverse=false) {
     n=newn;
   } while(n>0);
 }
+
+/**
+ * Implementation of HEAPSORT ALGORITHM
+ * This algorithm uses the binary heap structure
+ * The binary tree structure is implemented in the array as the following rule:
+ * - iParent(i) = floor(i - 1) / 2 ;
+ * - iLeftChild(i) = 2 * i + 1 ;
+ * - iRightChild(i) = 2 * i + 2 ;
+ * @param {Object[]} list - list of data to sort;
+ * @param {function(Object1, Object2)} operator - this function is used to determine which data is "greater" or "less",
+ * Returns true if Object1 is "greater" than Object2, otherwise returns false;
+ * @param {bool} [reverse=false] - this controls the order of the list,
+ * Set to false means the order is from "low" to "high", and vice versa;
+ */
+function heapSort(list, operator, reverse=false) {
+  // First we build the heap in the array so that the largest value is at the root
+  // <heapify>
+  var count=list.length; // store length of the list
+  // start is the last parent node of the list, so one of its children is in the last index (count-1)
+  var start=Math.floor((count-2)/2);  // iParent(count-1)
+  for(; start>=0; start--) {
+
+    // <siftDown>
+    var root=start;
+    while(2*root+1 < count) { // left child of root: iLeftChild(root)
+      var child=2*root+1; // left child of root
+      var swap=root;  // keeps track of child to swap with
+      // Now we find the largest element between the root, its left and right child
+      // Then swap the largest element with the root
+      if(operator(list[child], list[swap])!=reverse)  // Compare swap with the left child
+        swap=child; // swap may now be the index of the left child
+      if(child+1<count && (operator(list[child+1], list[swap]) != reverse)) // Compare swap with the right child if it exists
+        swap=child+1; // swap may now be the index of the right child
+      // Assume that the heaps rooted at its children are valid
+      if(swap==root)
+        break;  // No change happens
+      else {
+        //  swapping
+        var temp=list[root];
+        list[root]=list[swap];
+        list[swap]=temp;
+        root=swap;
+      }
+    }
+    // </siftDown>
+
+  }
+  // </heapify>
+
+  // Back to main
+  // The array consists of two parts: the heap and the sorted array
+  // The process above had created the heap with the size of the array
+  // Now we will create the sorted array by removing the root (the largest element of the heap) to the end of the array,
+  // where we place the sorted array, and continue the process until the array is fully sorted
+  var end=count-1;
+  while(end>0) {
+    // swapping the root of the heap to the front of the sorted array
+    var temp=list[end];
+    list[end]=list[0];
+    list[0]=temp;
+    // reduce the size of the heap
+    end--;
+    // restore the heap property
+    // <siftDown>
+    var root=0;
+    while(2*root+1 <= end) { // left child of root: iLeftChild(root)
+      var child=2*root+1; // left child of root
+      var swap=root;  // keeps track of child to swap with
+      // Now we find the largest element between the root, its left and right child
+      // Then swap the largest element with the root
+      if(operator(list[child], list[swap])!=reverse)  // Compare swap with the left child
+        swap=child; // swap may now be the index of the left child
+      if(child+1<=end && (operator(list[child+1], list[swap]) != reverse)) // Compare swap with the right child if it exists
+        swap=child+1; // swap may now be the index of the right child
+      // Assume that the heaps rooted at its children are valid
+      if(swap==root)
+        break;  // No change happens
+      else {
+        //  swapping
+        var temp=list[root];
+        list[root]=list[swap];
+        list[swap]=temp;
+        root=swap;
+      }
+    }
+    // </siftDown>
+  }
+}
--- mainJS.js
+++ mainJS.js
@@ -1,23 +1,51 @@
-$(document).ready(function() {
-  $.getJSON("Data/numbers.js", function(result) {
+var data = [];
+$.getJSON("Data/numbers.js", function(result) {
 
-        for(var i=0; i<result.array.length; i++) {
-          var row=$("<tr>").append($("<td>", {text : result.array[i] }));
-          $("#dataTable").append(row);
-        }
-        insertionSort(result.array, function(obj1, obj2){
-          return obj1>obj2;
-        }, true);
-        for(var i=0; i<result.array.length; i++) {
-          var cell=$("<td>", {text : result.array[i] });
-          $("tr:eq("+i+")").append(cell);
-        }
-        bubbleSort(result.array, function(obj1, obj2) {
-          return obj1>obj2;
-        });
-        for(var i=0; i<result.array.length; i++) {
-          var cell=$("<td>", {text : result.array[i] });
-          $("tr:eq("+i+")").append(cell);
-        }
-    });
+  for(var i=0; i<result.array.length; i++) {
+    data.push(result.array[i]);
+    var row=$("<tr>").append($("<td>", {text : result.array[i] }));
+    $("#dataTable").append(row);
+  }
+  var t1, t2;
+  var m=[];
+  for(var k=0; k<100; k++) {
+    data=[];
+    for(var i=0; i<result.array.length; i++) {
+      data.push(result.array[i]);
+    }
+  t1 = performance.now();
+  bubbleSort(data, function(obj1, obj2){
+    return obj1>obj2;
+  });
+  t1 = performance.now() - t1;
+
+  // for(var i=0; i<data.length; i++) {
+  //   var cell=$("<td>", {text : data[i] });
+  //   $("tr:eq("+i+")").append(cell);
+  // }
+
+  var data2=[];
+  for(var i=0; i<result.array.length; i++) {
+    data2.push(result.array[i]);
+  }
+
+  t2 = performance.now();
+  heapSort(data2, function(obj1, obj2) {
+    return obj1>obj2;
+  });
+  t2 = performance.now() - t2;
+  m.push(t2/t1);
+}
+var s=0;
+for(var i=0;i<m.length;i++)
+  s+=m[i];
+  s/=m.length;
+  console.log(s);
+  var b=true;
+  for(var i=0; i<data.length; i++) {
+    if(data[i]!=data2[i]) b=false;
+    var cell=$("<td>", {text : data2[i] });
+    $("tr:eq("+i+")").append(cell);
+  }
+  console.log(b);
 });
